731d18e4dab65cc60c461c6d5727858385217882
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2018-01-22 00:02:19 +0000

    CSS editor tweaks and fixes
    - Load CodeMirror locally
    - CSS tweaks - editor window look more native now
    - Fixed bug where the live update checkbox can’t be unchecked

--- core/src/modules/csseditor.js
+++ core/src/modules/csseditor.js
@@ -5,7 +5,7 @@
  * https://github.com/JsSucks - https://betterdiscord.net
  *
  * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree. 
+ * LICENSE file in the root directory of this source tree.
 */
 
 const path = require('path');
@@ -16,6 +16,8 @@ const { Module } = require('./modulebase');
 class CSSEditor extends Module {
 
     openEditor(o) {
+        if (this.editor && this.editor.isFocused()) return;
+
         if (this.editor && this.editor.open) {
             this.editor.focus();
             this.editor.flashFrame(true);
@@ -26,6 +28,7 @@ class CSSEditor extends Module {
         this.editor = new BrowserWindow(this.options);
         this.editor.loadURL(`file://${this.editorPath}/index.html`);
         this.editor.open = true;
+        this.editor.setSheetOffset(33);
 
         this.editor.webContents.on('close', () => {
             this.editor.open = false;
